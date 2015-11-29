8d83bc8100a5a3dc5a974d08cc5f811cf84b010f
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2015-11-29 08:20:55 +0000

    Tidyup

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -116,24 +116,9 @@
             </li>
             <!-- Menu Body -->
             <li class="user-body">
-              <div class="col-xs-4 text-center">
-                <a href="#">Followers</a>
-              </div>
-              <div class="col-xs-4 text-center">
-                <a href="#">Sales</a>
-              </div>
-              <div class="col-xs-4 text-center">
-                <a href="#">Friends</a>
-              </div>
-            </li>
-            <!-- Menu Footer-->
-            <li class="user-footer">
-              <div class="pull-left">
-                <a href="#" class="btn btn-default btn-flat">Profile</a>
-              </div>
-              <div class="pull-right">
-                <a href="#" class="btn btn-default btn-flat">Sign out</a>
-              </div>
+                <div class="pull-right">
+                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                </div>
             </li>
           </ul>
         </li>
